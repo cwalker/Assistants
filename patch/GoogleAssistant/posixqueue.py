dcb08159b77ea5d68ed60bff87b5cfb7055954fc
cwalker <arafikov@gmail.com>
2019-01-15 09:53:22 +0400

    Trying implement porcupine voice engine

--- patch/GoogleAssistant/posixqueue.py
+++ patch/GoogleAssistant/posixqueue.py
@@ -12,25 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Sample that implements a gRPC client for the Google Assistant API."""
+import os
+import platform
+import struct
+import sys
+from datetime import datetime
+from threading import Thread
+
+import numpy as np
+import pyaudio
+import soundfile
 
 import concurrent.futures
 import json
+import click
 import logging
+import pyperclip as clip
+import pyautogui
+import subprocess
 import os
 import os.path
 import pathlib2 as pathlib
 import sys
 import uuid
 import time
-import click
+
 import grpc
 import google.auth.transport.grpc
 import google.auth.transport.requests
 import google.oauth2.credentials
+import wave
+import time
 import posix_ipc
 import json
 from time import sleep
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'binding/python'))
+
+from porcupine import Porcupine
 
 from google.assistant.embedded.v1alpha2 import (
     embedded_assistant_pb2,
@@ -54,10 +73,42 @@ ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
 END_OF_UTTERANCE = embedded_assistant_pb2.AssistResponse.END_OF_UTTERANCE
 DIALOG_FOLLOW_ON = embedded_assistant_pb2.DialogStateOut.DIALOG_FOLLOW_ON
 CLOSE_MICROPHONE = embedded_assistant_pb2.DialogStateOut.CLOSE_MICROPHONE
+PLAYING = embedded_assistant_pb2.ScreenOutConfig.PLAYING
 DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
 google_mq = posix_ipc.MessageQueue("/GoogleAssistantQueue", posix_ipc.O_CREAT)
 asssitantControl_mq = posix_ipc.MessageQueue("/AssistantsControlQueue", posix_ipc.O_CREAT)
+TOP_DIR = os.path.dirname(os.path.abspath(__file__))
+DETECT_DING = os.path.join(TOP_DIR, "resources/ding.wav")
+DETECT_DONG = os.path.join(TOP_DIR, "resources/dong.wav")
 
+def no_alsa_error():
+    try:
+        asound = cdll.LoadLibrary('libasound.so')
+        asound.snd_lib_error_set_handler(c_error_handler)
+        yield
+        asound.snd_lib_error_set_handler(None)
+    except:
+        yield
+        pass
+
+def play_audio_file(fname=DETECT_DING):
+    ding_wav = wave.open(fname, 'rb')
+    ding_data = ding_wav.readframes(ding_wav.getnframes())
+    audio = pyaudio.PyAudio()
+    stream_out = audio.open(
+        format=audio.get_format_from_width(ding_wav.getsampwidth()),
+        channels=ding_wav.getnchannels(),
+        rate=ding_wav.getframerate(), input=False, output=True)
+    stream_out.start_stream()
+    stream_out.write(ding_data)
+    time.sleep(0.2)
+    stream_out.stop_stream()
+    stream_out.close()
+    audio.terminate()
+
+def msg(str):
+    #print("assist: " + str)
+    1
 class SampleAssistant(object):
     """Sample Assistant that supports conversations and device actions.
 
@@ -73,12 +124,13 @@ class SampleAssistant(object):
     """
 
     def __init__(self, language_code, device_model_id, device_id,
-                 conversation_stream,
+                 conversation_stream, display,
                  channel, deadline_sec, device_handler):
         self.language_code = language_code
         self.device_model_id = device_model_id
         self.device_id = device_id
         self.conversation_stream = conversation_stream
+        self.display = display
 
         # Opaque blob provided in AssistResponse that,
         # when provided in a follow-up AssistRequest,
@@ -213,6 +265,10 @@ class SampleAssistant(object):
                 device_model_id=self.device_model_id,
             )
         )
+        if self.display:
+            config.screen_out_config.screen_mode = PLAYING
+        # Continue current conversation with later requests.
+        self.is_new_conversation = False
         # The first AssistRequest must contain the AssistConfig
         # and no audio data.
         yield embedded_assistant_pb2.AssistRequest(config=config)
@@ -220,8 +276,178 @@ class SampleAssistant(object):
             # Subsequent requests need audio data, but not config.
             yield embedded_assistant_pb2.AssistRequest(audio_in=data)
 
+class PorcupineDemo(Thread):
+    """
+    Demo class for wake word detection (aka Porcupine) library. It creates an input audio stream from a microphone,
+    monitors it, and upon detecting the specified wake word(s) prints the detection time and index of wake word on
+    console. It optionally saves the recorded audio into a file for further review.
+    """
+
+    def __init__(
+            self,
+            lang, device_model_id, device_id,
+            conversation_stream, display,
+            grpc_channel, grpc_deadline,
+            device_handler,
+            library_path,
+            model_file_path,
+            keyword_file_paths,
+            sensitivities,
+            input_device_index=None,
+            output_path=None):
+
+        """
+        Constructor.
+
+        :param library_path: Absolute path to Porcupine's dynamic library.
+        :param model_file_path: Absolute path to the model parameter file.
+        :param keyword_file_paths: List of absolute paths to keyword files.
+        :param sensitivities: Sensitivity parameter for each wake word. For more information refer to
+        'include/pv_porcupine.h'. It uses the
+        same sensitivity value for all keywords.
+        :param input_device_index: Optional argument. If provided, audio is recorded from this input device. Otherwise,
+        the default audio input device is used.
+        :param output_path: If provided recorded audio will be stored in this location at the end of the run.
+        """
+
+        super(PorcupineDemo, self).__init__()
+
+        self._library_path = library_path
+        self._model_file_path = model_file_path
+        self._keyword_file_paths = keyword_file_paths
+        self._sensitivities = sensitivities
+        self._input_device_index = input_device_index
+
+        self._output_path = output_path
+        if self._output_path is not None:
+            self._recorded_frames = []
+
+        self._assistant = SampleAssistant(lang, device_model_id, device_id,
+                         conversation_stream, display,
+                         grpc_channel, grpc_deadline,
+                         device_handler)
+
+    def run(self):
+        """
+         Creates an input audio stream, initializes wake word detection (Porcupine) object, and monitors the audio
+         stream for occurrences of the wake word(s). It prints the time of detection for each occurrence and index of
+         wake word.
+         """
+
+        num_keywords = len(self._keyword_file_paths)
+
+        keyword_names =\
+            [os.path.basename(x).replace('.ppn', '').replace('_tiny', '').split('_')[0] for x in self._keyword_file_paths]
+
+        print('listening for:')
+        for keyword_name, sensitivity in zip(keyword_names, self._sensitivities):
+            print('- %s (sensitivity: %f)' % (keyword_name, sensitivity))
+
+        porcupine = None
+        pa = None
+        audio_stream = None
+        try:
+            porcupine = Porcupine(
+                library_path=self._library_path,
+                model_file_path=self._model_file_path,
+                keyword_file_paths=self._keyword_file_paths,
+                sensitivities=self._sensitivities)
+
+            pa = pyaudio.PyAudio()
+            audio_stream = pa.open(
+                rate=porcupine.sample_rate,
+                channels=1,
+                format=pyaudio.paInt16,
+                input=True,
+                frames_per_buffer=porcupine.frame_length,
+                input_device_index=self._input_device_index)
+
+            while True:
+                pcm = audio_stream.read(porcupine.frame_length)
+                pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+                if self._output_path is not None:
+                    self._recorded_frames.append(pcm)
+
+                result = porcupine.process(pcm)
+                if num_keywords == 1 and result:
+                    play_audio_file(DETECT_DING)
+                    print('[%s] detected keyword' % str(datetime.now()))
+                    while True:
+                        #if self._assistant.assistOrg() == False:
+                        if self._assistant.assist() == False:
+                            break
+                    #print('done')
+                    play_audio_file(DETECT_DONG)
+                elif num_keywords > 1 and result >= 0:
+                    print('[%s] detected %s' % (str(datetime.now()), keyword_names[result]))
+
+        except KeyboardInterrupt:
+            print('stopping ...')
+        finally:
+            if porcupine is not None:
+                porcupine.delete()
+
+            if audio_stream is not None:
+                audio_stream.close()
+
+            if pa is not None:
+                pa.terminate()
+
+            if self._output_path is not None and len(self._recorded_frames) > 0:
+                recorded_audio = np.concatenate(self._recorded_frames, axis=0).astype(np.int16)
+                #soundfile.write(self._output_path, recorded_audio, samplerate=porcupine.sample_rate, subtype='PCM_16')
+
+    _AUDIO_DEVICE_INFO_KEYS = ['index', 'name', 'defaultSampleRate', 'maxInputChannels']
+
+    @classmethod
+    def show_audio_devices_info(cls):
+        """ Provides information regarding different audio devices available. """
+
+        pa = pyaudio.PyAudio()
+
+        for i in range(pa.get_device_count()):
+            info = pa.get_device_info_by_index(i)
+            print(', '.join("'%s': '%s'" % (k, str(info[k])) for k in cls._AUDIO_DEVICE_INFO_KEYS))
+
+        pa.terminate()
+
+def _default_library_path():
+    system = platform.system()
+    machine = platform.machine()
+    if machine == 'i686':
+        machine = 'i386'
+    if system == 'Darwin':
+        return os.path.join(os.path.dirname(__file__), '../../lib/mac/%s/libpv_porcupine.dylib' % machine)
+    elif system == 'Linux':
+        #if machine == 'x86_64' or machine == 'i386':
+        if machine == 'x86_64' or machine == 'i386' or machine == 'i686':
+            #return os.path.join(os.path.dirname(__file__), '../../lib/linux/%s/libpv_porcupine.so' % machine)
+            return os.path.join(os.path.dirname(__file__), 'lib/linux/%s/libpv_porcupine.so' % machine)
+        else:
+            raise Exception('cannot autodetect the binary type. Please enter the path to the shared object using --library_path command line argument.')
+    elif system == 'Windows':
+        if platform.architecture()[0] == '32bit':
+            return os.path.join(os.path.dirname(__file__), '..\\..\\lib\\windows\\i686\\libpv_porcupine.dll')
+        else:
+            return os.path.join(os.path.dirname(__file__), '..\\..\\lib\\windows\\amd64\\libpv_porcupine.dll')
+    raise NotImplementedError('Porcupine is not supported on %s/%s yet!' % (system, machine))
+
 
 @click.command()
+@click.option('--keyword_file_paths', default=os.path.join(os.path.dirname(__file__), 'lib/common/porcupine_params.pv'), help='absolute path to model parameter file.')
+@click.option('--model_file_path', default=os.path.join(os.path.dirname(__file__), 'lib/common/porcupine_params.pv'),
+              help='comma-separated absolute paths to keyword files.')
+@click.option('--library_path', default=_default_library_path(),
+              help='absolute path to Porcupine dynamic library.')
+@click.option('--asensitivities', default=0.5,
+              help='detection sensitivity [0, 1].')
+@click.option('--input_audio_device_index', default=None,
+              help='index of input audio device.')
+@click.option('--output_path', default='',
+              help='absolute path to where recorded audio will be stored. If not set, it will be bypassed.')
+@click.option('--show_audio_devices_info', default='',
+              help='show_audio_devices_info.')
 @click.option('--api-endpoint', default=ASSISTANT_API_ENDPOINT,
               metavar='<api endpoint>', show_default=True,
               help='Address of Google Assistant API service.')
@@ -254,6 +480,8 @@ class SampleAssistant(object):
               metavar='<language code>',
               default='en-US',
               help='Language code of the Assistant')
+@click.option('--display', is_flag=True, default=False,
+              help='Enable visual display of Assistant responses in HTML.')
 @click.option('--verbose', '-v', is_flag=True, default=False,
               help='Verbose logging.')
 @click.option('--input-audio-file', '-i',
@@ -291,8 +519,11 @@ class SampleAssistant(object):
               help='gRPC deadline in seconds')
 @click.option('--once', default=False, is_flag=True,
               help='Force termination after a single conversation.')
-def main(api_endpoint, credentials, project_id,
-         device_model_id, device_id, device_config, lang, verbose,
+def main(keyword_file_paths, library_path, model_file_path, 
+         asensitivities, input_audio_device_index, output_path, show_audio_devices_info,
+         api_endpoint, credentials, project_id,
+         device_model_id, device_id, device_config,
+         lang, display, verbose,
          input_audio_file, output_audio_file,
          audio_sample_rate, audio_sample_width,
          audio_iter_size, audio_block_size, audio_flush_size,
@@ -428,9 +659,15 @@ def main(api_endpoint, credentials, project_id,
             logging.info('Turning device off')
 
     with SampleAssistant(lang, device_model_id, device_id,
-                         conversation_stream,
-                         grpc_channel, grpc_deadline,
-                         device_handler) as assistant:
+            conversation_stream, display,
+            grpc_channel, grpc_deadline,
+            device_handler,
+            library_path,
+            model_file_path,
+            keyword_file_paths,
+            sensitivities,
+            input_device_index=input_audio_device_index,
+            output_path=output_path) as assistant:
         # If file arguments are supplied:
         # exit after the first turn of the conversation.
         if input_audio_file or output_audio_file:
